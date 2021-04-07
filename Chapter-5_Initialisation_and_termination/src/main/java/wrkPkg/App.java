19d15844c94c8770ddc0f4cd6d053db182c7f3d9
Alexey Suetin <avsuetin@gmail.com>
2021-04-07 17:46:26 +0300

    Первый commit

---
+++ Chapter-5_Initialisation_and_termination/src/main/java/wrkPkg/App.java
@@ -0,0 +1,18 @@
+package wrkPkg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args){
+        List<String> items = new ArrayList<>();
+        items.add("Hello");
+        items.add(" ");
+        items.add("World");
+        items.add("!");
+        StringBuilder sb = new StringBuilder();
+        items.forEach(s -> sb.append(s));
+        System.out.println(sb.toString());
+    }
+
+}
