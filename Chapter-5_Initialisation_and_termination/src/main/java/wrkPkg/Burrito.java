476e992d4382e3ad09db66d114903e11ef85b53f
Alexey Suetin <avsuetin@gmail.com>
2021-04-16 12:10:06 +0300

    Фиксация

--- Chapter-5_Initialisation_and_termination/src/main/java/wrkPkg/Burrito.java
+++ Chapter-5_Initialisation_and_termination/src/main/java/wrkPkg/Burrito.java
@@ -6,31 +6,35 @@ enum Speciness {
 
 public class Burrito {
     Speciness degree;
-    public Burrito(Speciness degree){
+
+    public Burrito(Speciness degree) { //конструктор
         this.degree = degree;
     }
+
     public void describe() {
         System.out.print("This burrito is ");
         switch (degree) {
-            case HOT: System.out.println("not spicy at all.");
-                      break;
+            case HOT:
+                System.out.println("not spicy at all.");
+                break;
             case MILD:
-            case MEDIUM: System.out.println("a little hot.");
-                         break;
+            case MEDIUM:
+                System.out.println("a little hot.");
+                break;
             case NOT:
             case FLAMING:
-            default: System.out.println("maybe to hot.");
+            default:
+                System.out.println("maybe to hot.");
         }
     }
-public static void main(String[] args) {
-    Burrito
-    plain = new Burrito(Speciness.NOT),
-    greenChile = new Burrito(Speciness.MEDIUM),
-    jalapeno = new Burrito(Speciness.HOT);
-    plain.describe();
-    greenChile.describe();
-    jalapeno.describe();
-}
 
-
+    public static void main(String[] args) {
+        Burrito
+                plain = new Burrito(Speciness.NOT),
+                greenChile = new Burrito(Speciness.MEDIUM),
+                jalapeno = new Burrito(Speciness.HOT);
+        plain.describe();
+        greenChile.describe();
+        jalapeno.describe();
+    }
 }
