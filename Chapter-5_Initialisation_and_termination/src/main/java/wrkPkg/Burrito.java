e2468e77a075841425e73d73cd26d2cedcef16f3
Alexey Suetin <avsuetin@gmail.com>
2021-04-16 11:50:05 +0300

    Фиксация

---
+++ Chapter-5_Initialisation_and_termination/src/main/java/wrkPkg/Burrito.java
@@ -0,0 +1,36 @@
+package wrkPkg;
+
+public enum Speciness {
+    NOT, MILD, MEDIUM, HOT, FLAMING
+}
+
+public class Burrito {
+    Speciness degree;
+    public Burrito(Speciness degree){
+        this.degree = degree;
+    }
+    public void describe() {
+        System.out.print("This burrito is ");
+        switch (degree) {
+            case HOT: System.out.println("not spicy at all.");
+                      break;
+            case MILD:
+            case MEDIUM: System.out.println("a little hot.");
+                         break;
+            case NOT:
+            case FLAMING:
+            default: System.out.println("maybe to hot.");
+        }
+    }
+public static void main(String[] args) {
+    Burrito
+    plain = new Burrito(Speciness.NOT),
+    greenChile = new Burrito(Speciness.MEDIUM),
+    jalapeno = new Burrito((Speciness.HOT);
+    plain.describe();
+    greenChile.describe();
+    jalapeno.describe();
+}
+
+
+}
