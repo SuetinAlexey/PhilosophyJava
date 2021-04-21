3367f94308d727930097bdbd5ad139e9b423f74a
Alexey Suetin <avsuetin@gmail.com>
2021-04-21 17:04:04 +0300

    Фиксация

--- Chapter-8_Polymorphism/src/main/java/wrkPkg/Music2.java
+++ Chapter-8_Polymorphism/src/main/java/wrkPkg/Music2.java
@@ -1,13 +1,33 @@
 package wrkPkg;
 
-import wrkPkg.Instrument;
-import wrkPkg.Note;
-
 class Stringed extends Instrument {
     public void play(Note n) {
-        System.out.println("wrkPkg.Stringed.play()" + n);
+        System.out.println("Stringed.play()" + n);
+    }
+}
+
+class Brass extends Instrument {
+    public void play(Note n) {
+        System.out.println("Brass.play()" + n);
     }
 }
 
 public class Music2 {
+    public static void tune(Wind i){
+        i.play(Note.MIDDLE_C);
+    }
+    public static void tune(Stringed i){
+        i.play(Note.MIDDLE_C);
+    }
+    public static void tune(Brass i){
+        i.play(Note.MIDDLE_C);
+    }
+    public static void main(String[] args) {
+        Wind flute = new Wind();
+        Stringed violin = new Stringed();
+        Brass frenchHorn = new Brass();
+        tune(flute);
+        tune(frenchHorn);
+        tune(violin);
+    }
 }
