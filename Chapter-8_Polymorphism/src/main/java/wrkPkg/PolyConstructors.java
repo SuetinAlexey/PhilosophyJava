d0163c5287a97954c9cdba042288a84499f9cfa1
Alexey Suetin <avsuetin@gmail.com>
2021-04-22 14:42:26 +0300

    Фиксация

---
+++ Chapter-8_Polymorphism/src/main/java/wrkPkg/PolyConstructors.java
@@ -0,0 +1,31 @@
+package wrkPkg;
+// Конструкторы и полиморфизм дают не тот результат,
+// который можно было бы ожидать
+
+class Glyph{
+    void draw(){
+        System.out.println("Glyph.draw");
+    }
+    Glyph(){
+        System.out.println("Glyph перед вызовом draw");
+        draw();
+        System.out.println("Glyph после вызова draw");
+    }
+}
+
+class RoundGlyph extends Glyph {
+    private int radius = 1;
+    RoundGlyph(int r) {
+        radius = r;
+       System.out.println("RoundGlyph.RoundGlyph(), radius =  " + radius);
+    }
+    void draw() {
+        System.out.println("RoundGlyph.draw(), radius = " + radius);
+    }
+}
+
+public class PolyConstructors {
+    public static void main(String[] args){
+        new RoundGlyph(5);
+    }
+}
