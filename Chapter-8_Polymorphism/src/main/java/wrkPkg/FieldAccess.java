b636886e8f25d4792a45691c427e3d7ba2820c91
Alexey Suetin <avsuetin@gmail.com>
2021-04-22 13:04:38 +0300

    Фиксация

--- Chapter-8_Polymorphism/src/main/java/wrkPkg/FieldAccess.java
+++ Chapter-8_Polymorphism/src/main/java/wrkPkg/FieldAccess.java
@@ -17,6 +17,12 @@ class Sub extends Supper {
     }
 }
 
-
 public class FieldAccess {
+    public static void main(String[] args) {
+        Supper sup = new Sub(); // upcast
+        System.out.println("sup.field = " + sup.field + ", sup.GetField = " + sup.getField());
+        Sub sub = new Sub();
+        System.out.println("sub.field = " + sub.field + ", sub.GetField = " + sub.getField() +
+                "sub.getSupperField()" + sub.getSupperField());
+    }
 }
