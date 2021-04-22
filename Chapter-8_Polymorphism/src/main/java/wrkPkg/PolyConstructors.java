bd896ad889372b1e153d17a41b16fad337019560
Alexey Suetin <avsuetin@gmail.com>
2021-04-22 15:01:20 +0300

    Фиксация

--- Chapter-8_Polymorphism/src/main/java/wrkPkg/PolyConstructors.java
+++ Chapter-8_Polymorphism/src/main/java/wrkPkg/PolyConstructors.java
@@ -17,7 +17,7 @@ class RoundGlyph extends Glyph {
     private int radius = 1;
     RoundGlyph(int r) {
         radius = r;
-       System.out.println("RoundGlyph.RoundGlyph(), radius =  " + radius);
+        System.out.println("RoundGlyph.RoundGlyph(), radius =  " + radius);
     }
     void draw() {
         System.out.println("RoundGlyph.draw(), radius = " + radius);
