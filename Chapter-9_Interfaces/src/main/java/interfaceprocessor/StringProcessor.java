524c3443734704c1833fc12e85d137b001de9981
Alexey <avsuetin@gmail.com>
2021-04-25 12:12:04 +0300

    Фиксация

--- Chapter-9_Interfaces/src/main/java/interfaceprocessor/StringProcessor.java
+++ Chapter-9_Interfaces/src/main/java/interfaceprocessor/StringProcessor.java
@@ -1,12 +1,14 @@
 package interfaceprocessor;
 
 public abstract class StringProcessor implements Processor {
-    public String name(){
-        return getClass().getSimpleName();
-    }
-    public abstract String process();
+    public String name(){ return getClass().getSimpleName(); }
+    public abstract String process(Object input);
     public static String s = "If she weighs the same as a duck, she's made of wood";
-    public static void main(String[] args) {
-        Apply.process(new Upcase(), s);
+    public static void main(String[] args) { Apply.process(new Upcase(), s); }
+}
+
+class Upcase extends StringProcessor {
+    public String process (Object input) { // ковариантный возвращаемый тип
+        return ((String)input).toUpperCase();
     }
 }
