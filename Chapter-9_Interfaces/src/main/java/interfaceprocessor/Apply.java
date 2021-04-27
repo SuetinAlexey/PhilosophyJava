328fc5bec9f5535655acb4a040329a5e111c20a3
Alexey Suetin <avsuetin@gmail.com>
2021-04-27 13:49:28 +0300

    Фиксация

--- Chapter-9_Interfaces/src/main/java/interfaceprocessor/Apply.java
+++ Chapter-9_Interfaces/src/main/java/interfaceprocessor/Apply.java
@@ -1,7 +1,7 @@
 package interfaceprocessor;
 
 public class Apply {
-    public static void process (Processor p, Object s){
+    public static void process1 (Processor p, Object s){
         System.out.println("Using processor " + p.name());
         System.out.println(p.process(s));
     }
--- Chapter-9_Interfaces/src/main/java/interfaceprocessor/StringProcessor.java
+++ Chapter-9_Interfaces/src/main/java/interfaceprocessor/StringProcessor.java
@@ -4,11 +4,20 @@ public abstract class StringProcessor implements Processor {
     public String name(){ return getClass().getSimpleName(); }
     public abstract String process(Object input);
     public static String s = "If she weighs the same as a duck, she's made of wood";
-    public static void main(String[] args) { Apply.process(new Upcase(), s); }
+    public static void main(String[] args) {
+        Apply.process1(new Upcase(), s);
+        Apply.process1(new DownCase(), s);
+    }
 }
 
 class Upcase extends StringProcessor {
     public String process (Object input) { // ковариантный возвращаемый тип
         return ((String)input).toUpperCase();
+    }
+}
+
+class DownCase extends StringProcessor {
+    public String process (Object input) {
+        return ((String)input).toLowerCase();
     }
 }
