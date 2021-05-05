42b8d9bb0a61bb20c2623a6d116d16085cb42b91
Alexey Suetin <avsuetin@gmail.com>
2021-05-05 15:24:04 +0300

    Фиксация

---
+++ Chapter-10_Inner_classes/src/main/java/TestParcel.java
@@ -0,0 +1,33 @@
+class Parcel4 {
+    private class PContents implements Contents{
+        private int i = 11;
+        public int value() {
+            return i;
+        }
+    }
+    protected class PDestionation implements Destination {
+        private String label;
+        private PDestionation(String whereTo) {
+            label = whereTo;
+        }
+        public String readLabel() {
+            return label;
+        }
+    }
+    public Destination destination(String s){
+       return new PDestionation(s);
+    }
+    public Contents contents() {
+        return new PContents();
+    }
+}
+
+public class TestParcel {
+    public static void main(String args[]){
+        Parcel4 p = new Parcel4();
+        Contents c = p.contents();
+        Destination d = p.destination("Танзания");
+        //обращение к закрытому класу невозможно
+        //Parcel4.PContents pc = p.new PContents();
+    }
+}
