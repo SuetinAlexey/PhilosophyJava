46b93da85b361f77d006b2cca432b4a006686743
Alexey Suetin <avsuetin@gmail.com>
2021-05-05 17:20:03 +0300

    Фиксация

--- Chapter-10_Inner_classes/src/main/java/Parcel7b.java
+++ Chapter-10_Inner_classes/src/main/java/Parcel7b.java
@@ -1,3 +1,18 @@
-public class Parcel7b {
+// расширенная версия Parcel7
 
+public class Parcel7b {
+    class MyContents implements Contents{
+        private int i = 11;
+        public int value(){
+            return i;
+        }
+    }
+    public Contents contents(){
+        return new MyContents();
+    }
+    public static void main(String[] args){
+        Parcel7b p = new Parcel7b();
+        Contents c = p.contents();
+        System.out.println(c.value());
+    }
 }
