131a74e1bade99b4ab112acaa2bea8562fc4f632
Alexey Suetin <avsuetin@gmail.com>
2021-05-06 13:48:56 +0300

    Фиксация

---
+++ Chapter-10_Inner_classes/src/main/java/MultiNestingAccess.java
@@ -0,0 +1,29 @@
+// Вложенные классы могут обращаться ко всем членам
+// всех уровней классов, в которые они вложены.
+
+class MNA{
+    private void f(){
+        System.out.println("MNA f()");
+    }
+    class A{
+        private void g(){
+            System.out.println("MNA.A g()");
+        }
+        public class B{
+            void h(){
+                System.out.println("MNA.A.B h()");
+                g();
+                f();
+            }
+        }
+    }
+}
+
+public class MultiNestingAccess {
+    public static void main(String args){
+        MNA mna = new MNA();
+        MNA.A mnaa = mna.new A();
+        MNA.A.B mnaab = mnaa.new B();
+        mnaab.h();
+    }
+}
