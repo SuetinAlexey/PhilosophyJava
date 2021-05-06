8b092f6ca45a92f8eaba5067685a73a4944eb2c4
Alexey Suetin <avsuetin@gmail.com>
2021-05-06 16:52:09 +0300

    Фиксация

---
+++ Chapter-10_Inner_classes/src/main/java/сontroller/Event.java
@@ -0,0 +1,17 @@
+package сontroller;
+
+public abstract class Event {
+    private long eventTime;
+    protected final long delayTime;
+    public Event(long delayTime){
+        this.delayTime = delayTime;
+        start();
+    }
+    public void start(){ // с возможностью перезапуска
+        eventTime = System.nanoTime() + delayTime;
+    }
+    public boolean ready(){
+        return System.nanoTime() >= eventTime;
+    }
+    public abstract void action();
+}
