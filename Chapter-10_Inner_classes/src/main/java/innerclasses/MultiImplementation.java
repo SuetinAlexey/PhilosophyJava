b5b5aea6ce8c8e44748362ce3f2df027f2d27205
Alexey Suetin <avsuetin@gmail.com>
2021-05-06 14:42:44 +0300

    Фиксация

---
+++ Chapter-10_Inner_classes/src/main/java/innerclasses/MultiImplementation.java
@@ -0,0 +1,23 @@
+// При использовании конкретных или абстрактных классов
+// внутренние классы предоставляют единственный способ
+// провести "множественное наследование реализации".
+
+package innerclasses;
+
+class D{}
+abstract class E{}
+class Z extends D{
+    E makeE(){
+        return new E() { };
+    }
+}
+
+public class MultiImplementation {
+    static void takesD(D d) {}
+    static void takesE(E e) {}
+    public static void main(String[] args){
+        Z z = new Z();
+        takesD(z);
+        takesE(z.makeE());
+    }
+}
