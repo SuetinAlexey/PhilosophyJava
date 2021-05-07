194a829c1bdcacec52024b0452eb3c630863c33b
Alexey Suetin <avsuetin@gmail.com>
2021-05-07 14:55:59 +0300

    Фиксация

--- Chapter-10_Inner_classes/src/main/java/BigEgg2.java
+++ Chapter-10_Inner_classes/src/main/java/BigEgg2.java
@@ -21,6 +21,21 @@ class Egg2{
     }
 }
 
-public class BigEgg2 {
+public class BigEgg2 extends Egg2 {
+    public class Yolk extends Egg2.Yolk{
+        public Yolk(){
+            System.out.println("BigEgg2.Yolk");
+        }
+        public void f(){
+            System.out.println("BigEgg2.Yolk.f()");
+        }
+    }
+    public BigEgg2(){
+        insertYolk(new Yolk());
+    }
+    public static void main(String[] args){
+        Egg2 e2 = new BigEgg2();
+       // Egg2.g();
+    }
 
 }
