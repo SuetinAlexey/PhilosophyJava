244b053f892e4139315c0ee06d738b8d7cededaa
Alexey Suetin <avsuetin@gmail.com>
2021-05-07 11:10:04 +0300

    Фиксация

---
+++ Chapter-7_Reusing_classes/src/main/java/Chess.java
@@ -0,0 +1,24 @@
+// Наследование, конструкторы и аргументы,
+
+class Game{
+    Game(int i){
+        System.out.println("Конструктор Game");
+    }
+}
+
+class BoardGame extends Game{
+    BoardGame(int i){
+        super(i);
+        System.out.println("Конструткор BoardGame");
+    }
+}
+
+public class Chess extends BoardGame {
+    Chess(){
+        super(11);
+        System.out.println("Конструктор Chess");
+    }
+    public static void main(String[] args){
+        Chess x = new Chess();
+    }
+}
