988ea925d5e78a7d4a6d0b7b41b3643dc2fcafd7
Alexey Suetin <avsuetin@gmail.com>
2021-05-26 15:45:33 +0300

    Фиксация

---
+++ Chapter-14_Typeinfo/src/main/java/pets/Individual.java
@@ -0,0 +1,10 @@
+package pets;
+
+public class Individual {
+    Integer id;
+    private String name;
+    public Individual() {};
+    public Individual(String name) {
+        this.name = name;
+    }
+}
