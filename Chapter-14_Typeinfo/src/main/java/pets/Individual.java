3206cb98b146efcebc5e2dab41221de4c35f93f0
Alexey Suetin <avsuetin@gmail.com>
2021-05-26 16:25:43 +0300

    Фиксация

--- Chapter-14_Typeinfo/src/main/java/pets/Individual.java
+++ Chapter-14_Typeinfo/src/main/java/pets/Individual.java
@@ -1,10 +1,16 @@
 package pets;
 
 public class Individual {
-    Integer id;
+    private static long counter = 0;
+    private final long id = counter++;
     private String name;
     public Individual() {};
     public Individual(String name) {
         this.name = name;
     }
+    public String toString(){
+        return getClass().getSimpleName()+(name == null ? " ": " " + name );
+    }
+    public long id(){return id;}
+
 }
