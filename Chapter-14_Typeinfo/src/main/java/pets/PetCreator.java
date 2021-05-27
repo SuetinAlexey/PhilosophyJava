b796d57208a01ab53896533bd2ab9bbb7e35df0d
Alexey Suetin <avsuetin@gmail.com>
2021-05-27 16:59:41 +0300

    Фиксация

--- Chapter-14_Typeinfo/src/main/java/pets/PetCreator.java
+++ Chapter-14_Typeinfo/src/main/java/pets/PetCreator.java
@@ -1,4 +1,32 @@
 package pets;
 
+import javafx.print.Collation;
+
+import java.util.*;
+
 public abstract class PetCreator {
+    private Random rand = new Random(47);
+    // контерйнер List с разными видами создаваемых объектов Pet:
+    public abstract List<Class<? extends Pet>> types();
+    public Pet randomPet(){ // Создание одного случайного объекта Pet
+        int n = rand.nextInt(types().size());
+        try {
+            return types().get(n).newInstance();
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException();
+        }
+    }
+    public Pet[] createArray(int size){
+        Pet[] result = new Pet[size];
+        for (int i = 0; i < size; i++)
+            result[i] = randomPet();
+        return  result;
+    }
+    public ArrayList<Pet> arrayList(int size){
+        ArrayList<Pet> result = new ArrayList<Pet>();
+        Collections.addAll(result, createArray(size));
+        return result;
+    }
 }
